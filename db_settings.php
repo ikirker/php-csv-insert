f88a26e86aac49ffb068bacac99fa5250490cf7e
Ian Kirker <ian.kirker@gmail.com>
2013-08-23 12:05:34 +0100

    Added actual db settings.

--- db_settings.php
+++ db_settings.php
@@ -2,12 +2,13 @@
     // (c) Ian Kirker, 2013
     // Released under the two-clause BSD license, in case it's relevant.
 
-    $my_db_hostname = "127.0.0.1";
-    $my_db_name     = "test";
+    $my_db_hostname = "mysql-server.ucl.ac.uk";
+    $my_db_name     = "ucqacpo";
     $my_db_port     = "3306";
-    $my_db_username = "root";
+    $my_db_username = "ucqacpo";
     $my_db_password = ""; // I am honestly not sure where/how to specify passwords.
-                                       // Putting them here seems like vaguely bad practice.
+    // Putting them here seems like vaguely bad practice.
+    // Certainly they shouldn't be put into a public repo.
 
 
 ?>
