5c61ad0ea59e75225844d77730517ceb54bed667
Ian Kirker <ian.kirker@gmail.com>
2013-09-09 18:40:33 +0100

    Globalised variables.

--- db_settings.php
+++ db_settings.php
@@ -8,7 +8,7 @@
     $my_db_username = "root";
     $my_db_password = ""; // I am honestly not sure where/how to specify passwords.
                                        // Putting them here seems like vaguely bad practice.
-
+    global $my_db_hostname, $my_db_name, $my_db_port, $my_db_username, $my_db_password;
 
 ?>
 
