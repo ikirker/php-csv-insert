f3f4d5cecaf9becd11d129ca3e3c3f5ca6b48e5f
Ian Kirker <ian.kirker@gmail.com>
2013-09-20 11:33:02 +0100

    Less worry

--- db_settings.php
+++ db_settings.php
@@ -10,8 +10,7 @@
       $my_db_port     = "3306";
       $my_db_username = "ucqacpo";
       $my_db_password = ""; // I am honestly not sure where/how to specify passwords.
-      // Putting them here seems like vaguely bad practice.
-      // Certainly they shouldn't be put into a public repo.
+                            // Certainly they shouldn't be put into a public repo.
     } elseif ($settings == "ian-testing") {
       $my_db_hostname = "127.0.0.1";
       $my_db_name     = "test";
